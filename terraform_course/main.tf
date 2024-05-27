3817a7b8b869b411554c4e2a3f8eaac7b7cc0808
kayodeao <115352923+kayodeao@users.noreply.github.com>
2024-05-27 13:30:21 +0100

    Update main.tf

--- terraform_course/main.tf
+++ terraform_course/main.tf
@@ -1,8 +1,9 @@
+# Define an S3 bucket resource named "mybucket"
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
- 
 }
 
+# Define ownership controls for the S3 bucket
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
@@ -11,6 +12,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
+# Define public access block settings for the S3 bucket
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
 
@@ -20,6 +22,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
+# Define access control list (ACL) for the S3 bucket
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [
     aws_s3_bucket_ownership_controls.example,
@@ -30,30 +33,33 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "public-read"
 }
 
+# Define an S3 object resource for the index.html file
 resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.mybucket.id
-  key    = "index.html"
-  source = "index.html"
-  acl    = "public-read"
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "index.html"
+  source       = "index.html"
+  acl          = "public-read"
   content_type = "text/html"
 }
 
+# Define an S3 object resource for the error.html file
 resource "aws_s3_object" "error" {
-  bucket = aws_s3_bucket.mybucket.id
-  key    = "error.html"
-  source = "error.html"
-  acl    = "public-read"
+  bucket       = aws_s3_bucket.mybucket.id
+  key          = "error.html"
+  source       = "error.html"
+  acl          = "public-read"
   content_type = "text/html"
 }
 
+# Define an S3 object resource for the profile image
 resource "aws_s3_object" "profile" {
   bucket = aws_s3_bucket.mybucket.id
   key    = "19CG026396b (1).jpg"
   source = "19CG026396b (1).jpg"
   acl    = "public-read"
-  
 }
 
+# Define website configuration for the S3 bucket
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.mybucket.id
 
@@ -65,5 +71,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
-  depends_on = [ aws_s3_bucket_acl.example ]
+  # Ensure ACLs are applied before configuring website
+  depends_on = [aws_s3_bucket_acl.example]
 }
